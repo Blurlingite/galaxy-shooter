9ef99b5d8a6c647177398536be436ba82840443e
Vishnu Dawah <vkdawah@gmail.com>
2019-08-13 15:41:23 -0400

    Added explosion sound when enemies and asteroids are destroyed

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+
+  [SerializeField]
+  private AudioClip _explosionSound;
+  private AudioSource _audioSource;
   // Start is called before the first frame update
   void Start()
   {
+
+    _audioSource = GetComponent<AudioSource>();
+
+    if (_audioSource == null)
+    {
+      Debug.LogError("Audio Source on Explosion Prefab is NULL");
+    }
+    else
+    {
+      _audioSource.clip = _explosionSound;
+    }
+
+    _audioSource.Play();
+
+
     // destroy explosion after 3 secs so it doesn't clutter up the Hierarchy in Unity 9b/c it is still there even if the asteroid is destroyed and even after the animation is done
     Destroy(this.gameObject, 3f);
   }
