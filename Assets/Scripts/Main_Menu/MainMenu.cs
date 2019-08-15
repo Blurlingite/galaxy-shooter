b41af358e87ed2a6ef0977ba4656d120f63cacd3
Vishnu Dawah <vkdawah@gmail.com>
2019-08-15 11:32:36 -0400

    Added a Co-Op button on main menu that leads to Co-Op scene when pressed

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class MainMenu : MonoBehaviour
-{
-  public void LoadGame()
-  {
+public class MainMenu : MonoBehaviour {
+  public void LoadGame () {
     // Start the game by loading the Game scene (which now has an index of 1 after re-dropping it into the Build Settings in Unity)
-    SceneManager.LoadScene(1);
+    SceneManager.LoadScene (1);
+  }
+
+  public void LoadCoopGame () {
+
+    SceneManager.LoadScene (2);
   }
 }
