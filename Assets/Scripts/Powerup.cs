9662929e4a5c810381114c5628747c0825952b9b
Vishnu Dawah <vkdawah@gmail.com>
2019-08-21 12:07:41 -0400

    All power ups that have a timed power down effect now have their effect time reset when collecting the same power up before the 1st one wears off

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Powerup : MonoBehaviour
-{
+public class Powerup : MonoBehaviour {
   [SerializeField]
   private float _speed = 3.0f;
   [SerializeField]
@@ -16,82 +15,73 @@ public class Powerup : MonoBehaviour
 
   private AudioSource _audioSource;
 
-  void Start()
-  {
-    _audioSource = GameObject.Find("Audio_Manager").transform.GetChild(1).GetComponent<AudioSource>();
+  void Start () {
+    _audioSource = GameObject.Find ("Audio_Manager").transform.GetChild (1).GetComponent<AudioSource> ();
 
-    if (_audioSource == null)
-    {
-      Debug.LogError("Audio Source on Powerup is NULL");
+    if (_audioSource == null) {
+      Debug.LogError ("Audio Source on Powerup is NULL");
     }
 
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update () {
     // Move down at the speed of 3m/s
-    transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    transform.Translate (Vector3.down * _speed * Time.deltaTime);
 
     // if we hit the bottom of screen, destroy us
-    if (transform.position.y < -4.5f)
-    {
-      Destroy(this.gameObject);
+    if (transform.position.y < -4.5f) {
+      Destroy (this.gameObject);
     }
 
   }
 
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-    if (other.gameObject.CompareTag("Player"))
-    {
-      Player player = other.transform.GetComponent<Player>();
+  private void OnTriggerEnter2D (Collider2D other) {
+    if (other.gameObject.CompareTag ("Player")) {
+      Player player = other.transform.GetComponent<Player> ();
 
-      if (player == null)
-      {
-        Debug.LogError("Powerup cannot find Player");
-      }
-      else
-      {
-        switch (powerupID)
-        {
+      if (player == null) {
+        Debug.LogError ("Powerup cannot find Player");
+      } else {
+        switch (powerupID) {
           // if powerupID = 0 enable triple shot
           case 0:
-            if (player.getIsTripleShotActive() == true)
-            {
-              player.setIsAnotherTripleShot();
+            if (player.getIsTripleShotActive () == true) {
+              player.setIsAnotherTripleShot ();
             }
-            player.TripleShotActive();
+            player.TripleShotActive ();
             break;
-          // if 1 enable speed powerup
+            // if 1 enable speed powerup
 
           case 1:
-            if (player.getIsSpeedBoostActive() == true)
-            {
-              player.setIsAnotherSpeedBoost();
+            if (player.getIsSpeedBoostActive () == true) {
+              player.setIsAnotherSpeedBoost ();
             }
-            player.SpeedBoostActive();
+            player.SpeedBoostActive ();
             // Debug.Log("SPEED IS KEY");
             break;
 
-          // if 2 enable shield powerup
+            // if 2 enable shield powerup
 
           case 2:
-            player.ShieldsActive();
+            player.ShieldsActive ();
             break;
           case 3:
-            player.ReflectorActive();
+            if (player.getIsReflectorActive () == true) {
+              player.setIsAnotherReflector ();
+            }
+            player.ReflectorActive ();
             break;
           default:
-            Debug.Log("Default Value");
+            Debug.Log ("Default Value");
             break;
 
         }
 
       }
-      _audioSource.Play();
+      _audioSource.Play ();
       // destroy the powerup giving the illusion that we collected it
-      Destroy(this.gameObject);
+      Destroy (this.gameObject);
     }
 
   }
