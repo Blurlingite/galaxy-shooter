a161971f427d72bf7cef880b5547fbf3a5cedd2b
Vishnu Dawah <vkdawah@gmail.com>
2019-08-22 20:36:20 -0400

    Working on adding more lives for players

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
   [SerializeField]
   private bool _isGameOver; // false by default
 
@@ -16,57 +17,68 @@ public class GameManager : MonoBehaviour {
   [SerializeField]
   private GameObject _pauseMenuPanel;
 
-  private void Start () {
+  private void Start()
+  {
     // check which Scene we're in by index, and if we are in Coop Mode change bool to true
 
     // When game starts let everything run at normal speed by setting Time.timeScale to 1.0f
     // This fixed an erro where starting a game from the main menu would make the game freeze b/c I set Time.timeScale to 0 somewhere else in my code. If I had never used Time.timeScale this error would not occur, but I needed to use it to enable pausing of the game
     Time.timeScale = 1.0f;
-    currentScene = SceneManager.GetActiveScene ();
+    currentScene = SceneManager.GetActiveScene();
 
-    if (currentScene == null) {
-      Debug.LogError ("Scene is NULL ::GameManager::Start()");
+    if (currentScene == null)
+    {
+      Debug.LogError("Scene is NULL ::GameManager::Start()");
     }
 
     int sceneIndex = currentScene.buildIndex;
-    if (sceneIndex == 2) {
+    if (sceneIndex == 2)
+    {
       isCoopMode = true;
     }
 
-    // _spawnManager = GameObject.Find ("Spawn_Manager").GetComponent<SpawnManager> ();
 
   }
 
-  private void Update () {
+  private void Update()
+  {
 
     // if the r key is pressed when game is over in single layer mode, restart the game by reloading the current scene
-    if (Input.GetKeyDown (KeyCode.R) && _isGameOver == true && isCoopMode == false) {
+    if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true && isCoopMode == false)
+    {
       // we can reload the scene by it's number or it's name. We named our game "Game" as you can see in the "Scenes" folder in Unity or by it's index (which you can see by adding the Scene to your Build Settings in Unity)
-      SceneManager.LoadScene (1);
+      SceneManager.LoadScene(1);
 
       // else if we're in Coop mode, reload the Coop scene 
-    } else if (Input.GetKeyDown (KeyCode.R) && _isGameOver == true && isCoopMode == true) {
-      SceneManager.LoadScene (2); // 
+    }
+    else if (Input.GetKeyDown(KeyCode.R) && _isGameOver == true && isCoopMode == true)
+    {
+      SceneManager.LoadScene(2); // 
 
     }
 
     // if ESCAPE key is pressed, quit the application
-    if (Input.GetKeyDown (KeyCode.Escape)) {
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
       // This won't work in the editor, so you have to make a new build to test this out. It should allow you to exit fullscreen
       // There is a way to quit it in the editor but is pointless if you are going to upload your game somewhere anyways (b/c it won't work on where you upload to)
-      Application.Quit ();
+      Application.Quit();
     }
 
     // if P key is pressed while unpaused, pause the game & enable pause menu. If pressed while paused, unpause & diable pause menu
 
-    if (Input.GetKeyDown (KeyCode.P)) {
+    if (Input.GetKeyDown(KeyCode.P))
+    {
 
-      if (_isGamePaused == false) {
+      if (_isGamePaused == false)
+      {
 
-        Pause ();
+        Pause();
 
-      } else if (_isGamePaused == true) {
-        Unpause ();
+      }
+      else if (_isGamePaused == true)
+      {
+        Unpause();
 
       }
 
@@ -74,26 +86,30 @@ public class GameManager : MonoBehaviour {
 
   }
 
-  public void GameOver () {
+  public void GameOver()
+  {
     _isGameOver = true;
   }
 
-  private void Pause () {
+  private void Pause()
+  {
     // controls the speed at which the game is running. if set to 0, the everything (that is frame dependent) will freeze. If set to a number between 0 and 1, the game will be in slow motion
     _isGamePaused = true;
     Time.timeScale = 0f;
-    _pauseMenuPanel.SetActive (true);
+    _pauseMenuPanel.SetActive(true);
   }
 
   // this is public, so we can also press the resume button to unpause the game
-  public void Unpause () {
+  public void Unpause()
+  {
     _isGamePaused = false;
     Time.timeScale = 1.0f;
-    _pauseMenuPanel.SetActive (false);
+    _pauseMenuPanel.SetActive(false);
   }
 
-  public void returnToMainMenu () {
-    SceneManager.LoadScene (0);
+  public void returnToMainMenu()
+  {
+    SceneManager.LoadScene(0);
   }
 
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; // used to communicate with UI elements in Unity
 
-public class UIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour
+{
   // we needed to bring in UnityEngine.UI (above) to have the Text variable since it's a UI element
   [SerializeField]
   private Text _scoreText, _bestScoreText;
@@ -16,6 +17,10 @@ public class UIManager : MonoBehaviour {
   [SerializeField]
   private Image _LivesImg;
   // Sprites to alternate between depending on how many lives left (assign the sprites in the Inspector in Unity by changing the size of this field to 4 and dragging & dropping the 4 sprites)
+
+  // used to get the index of 4 to show the "more than 3 lives" sprite in _liveSprites without having to set the currentlives to 4 (If you had 7 lives, it would be set to 4)
+  private int multiLives;
+
   [SerializeField]
   private Sprite[] _liveSprites;
 
@@ -27,93 +32,128 @@ public class UIManager : MonoBehaviour {
 
   private GameManager _gameManager;
 
-  void Start () {
+  [SerializeField]
+  private Text multiLivesText;
+
+  void Start()
+  {
     // assign text component to the handle so when game starts, there is a score that can be updated
     _scoreText.text = "Score: " + 0;
     // set this object to false to not display "Game Over" at the start of the game. Since _gameOverText is not a GameObject type, we must put .gameObject first
 
     // Retrieve the best score that was saved under the key "HighScore" (in CheckForBestScore()). If it's the first game and there is none saved, we default it to 0 by passing that as the 2nd argument
-    _bestScore = PlayerPrefs.GetInt ("HighScore", 0);
+    _bestScore = PlayerPrefs.GetInt("HighScore", 0);
 
     // actually assign the text field the loaded best score
     _bestScoreText.text = "Best: " + _bestScore;
 
-    _gameOverText.gameObject.SetActive (false);
+    _gameOverText.gameObject.SetActive(false);
 
-    _gameManager = GameObject.Find ("Game_Manager").GetComponent<GameManager> ();
+    _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
 
-    if (_gameManager == null) {
-      Debug.LogError ("Game Manager is NULL");
+    if (_gameManager == null)
+    {
+      Debug.LogError("Game Manager is NULL");
     }
   }
 
   // Update is called once per frame
-  public void UpdateScore (int playerScore) {
+  public void UpdateScore(int playerScore)
+  {
     // don't forget to put ".text" b/c you can't assign anything to just _scoreText
     _scoreText.text = "Score: " + playerScore;
   }
 
-  public void CheckForBestScore (int currentScore) {
-    if (currentScore > _bestScore) {
+  public void CheckForBestScore(int currentScore)
+  {
+    if (currentScore > _bestScore)
+    {
       _bestScore = currentScore;
 
       // use PlayerPrefs to save the score under the key "HighScore" You will use that key when you want to load the data (best score)
-      PlayerPrefs.SetInt ("HighScore", _bestScore);
+      PlayerPrefs.SetInt("HighScore", _bestScore);
       _bestScoreText.text = "Best: " + _bestScore;
 
     }
   }
 
-  public void UpdateLives (int currentLives) {
+  public void UpdateLives(int currentLives)
+  {
 
-    if (currentLives < 0) {
-      currentLives = 0;
+    if (currentLives < 0)
+    {
+      currentLives = 0; // prevent negative lives
     }
-    // access display image sprite and give it a new one based on currentLives
-    _LivesImg.sprite = _liveSprites[currentLives];
 
     // when out of lives, GameOverSequence() will display game over text by setting it's object active. This code should NOT go in the Player script b/c displaying UI things is the UI Manager's job
-    if (currentLives < 1) {
-      GameOverSequence ();
+    else if (currentLives < 1)
+    {
+      GameOverSequence();
     }
+
+    // Sprite Assignment
+
+    if (currentLives > 4)
+    {
+      multiLives = 4; // so we get the correct sprite at index 4 to represent more than 3 lives
+
+      // show multiple lives sprite
+      _LivesImg.sprite = _liveSprites[multiLives];
+      // set multiple lives text active
+      multiLivesText.gameObject.SetActive(true);
+      // Assign amount of lives
+      multiLivesText.text = "x " + currentLives;
+
+    }
+    else
+    {
+      // access display image sprite and give it a new one based on currentLives
+      _LivesImg.sprite = _liveSprites[currentLives];
+    }
+
   }
 
   // all code for a Game Over is in here
-  void GameOverSequence () {
+  void GameOverSequence()
+  {
     // notify Game Manager that the game is over so it can trigger a restart when player presses the "R" key
-    _gameManager.GameOver ();
-    _gameOverText.gameObject.SetActive (true);
+    _gameManager.GameOver();
+    _gameOverText.gameObject.SetActive(true);
     // Show restart text
-    _restartText.gameObject.SetActive (true);
+    _restartText.gameObject.SetActive(true);
 
-    StartCoroutine (GameOverFlicker ());
+    StartCoroutine(GameOverFlicker());
   }
 
   // causes the flickering Game Over effect
-  IEnumerator GameOverFlicker () {
+  IEnumerator GameOverFlicker()
+  {
 
-    while (true) {
+    while (true)
+    {
       // turn off 
-      _gameOverText.gameObject.SetActive (false);
+      _gameOverText.gameObject.SetActive(false);
 
       // wait half a second
-      yield return new WaitForSeconds (0.5f);
+      yield return new WaitForSeconds(0.5f);
 
       // turn on 
-      _gameOverText.gameObject.SetActive (true);
+      _gameOverText.gameObject.SetActive(true);
 
       // wait half a second (the flicker won't work if there's no wait time here so don't forget to add it )
-      yield return new WaitForSeconds (0.5f);
+      yield return new WaitForSeconds(0.5f);
 
     }
   }
 
-  public void ResumeGame () {
-    _gameManager.Unpause ();
+  public void ResumeGame()
+  {
+    _gameManager.Unpause();
   }
 
-  public void backToMainMenu () {
-    _gameManager.returnToMainMenu ();
+  public void backToMainMenu()
+  {
+    _gameManager.returnToMainMenu();
   }
 
 }
